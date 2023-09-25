18700d32f52b008bbd2bae3ac9ac361734e8ee40
dishapradhan81 <117260610+dishapradhan81@users.noreply.github.com>
2023-09-25 13:15:45 +0530

    Update ProjectList.js

--- portfolio/src/Components/ProjectList.js
+++ portfolio/src/Components/ProjectList.js
@@ -1,26 +1,45 @@
 import Proj3 from "../images/proj3.webp";
 import Proj1 from "../images/proj1.png";
 import Proj2 from "../images/proj2.png";
-
+import Proj4 from "../images/Proj4.jpg";
+import Proj6 from "../images/Proj6.jpg";
+import youtube from "../images/youtube.png";
 
 export const ProjectList = [
+  {
+    name: "Youtube Clone",
+    image: youtube,
+    skills: "MERN stack",
+    githubLink: "https://github.com/dishapradhan81/disha-youtube-clone",
+  },
   {
     name: "Contact Manager App",
     image: Proj1,
     skills: "React",
-    githubLink: "https://github.com/dishapradhan81/contact-manager-app-react"
+    githubLink: "https://github.com/dishapradhan81/contact-manager-app-react",
+  },
+  {
+    name: "Tenzi Dice Game",
+    image: Proj4,
+    skills: "React",
+    githubLink: "https://github.com/dishapradhan81/tenzi-dice-game-app-react",
+  },
+  {
+    name: "Meme Generator App",
+    image: Proj6,
+    skills: "React",
+    githubLink: "https://github.com/dishapradhan81/meme-generator-app-react",
   },
   {
     name: "Portfolio Website",
     image: Proj2,
     skills: "React",
-    githubLink: "https://github.com/dishapradhan81/my-portfolio"
+    githubLink: "https://github.com/dishapradhan81/my-portfolio",
   },
   {
     name: "The Food Website",
     image: Proj3,
     skills: "JavaScript,HTML,CSS",
-    githubLink: "https://github.com/dishapradhan81/The-Food-App"
+    githubLink: "https://github.com/dishapradhan81/The-Food-App",
   },
-  
 ];
