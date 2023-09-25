2b64e1201054abfb3fdff7c665c2de04c576402e
dishapradhan81 <117260610+dishapradhan81@users.noreply.github.com>
2023-09-25 13:18:28 +0530

    Update Navbar.js

--- portfolio/src/Pages/Navbar.js
+++ portfolio/src/Pages/Navbar.js
@@ -1,36 +1,48 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import  "../Styles/Navbar.css";
+import "../Styles/Navbar.css";
 import ReorderIcon from "@mui/icons-material/Reorder";
 
 function Navbar() {
-    const [expandNavbar, setExpandNavbar] = useState(false);
+  const [expandNavbar, setExpandNavbar] = useState(false);
 
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        setExpandNavbar(false);
-    }, [location]);
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
 
-    return ( 
+  return (
     <div className="navbar" id={expandNavbar ? "open" : "close"}>
-        <div className="toggleButton">
-            <button onClick={ () => {
-                setExpandNavbar((prev) => !prev);
-            }}> 
-                <ReorderIcon /> 
-                </button>
-
-        </div>
-        <div className="Links">
-            <Link to="/"> Home </Link>
-            <Link to="/projects"> Projects </Link>
-            <Link to="/education"> Education </Link>
-
-        </div>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="Links">
+        <Link className="desktopMenuListItem" to="/">
+          {" "}
+          Home{" "}
+        </Link>
+        <Link className="desktopMenuListItem" to="/projects">
+          {" "}
+          Projects{" "}
+        </Link>
+        <Link className="desktopMenuListItem" to="/education">
+          {" "}
+          Education{" "}
+        </Link>
+        <Link className="desktopMenuListItem" to="/contact">
+          {" "}
+          Contact{" "}
+        </Link>
+      </div>
     </div>
-    );
-
+  );
 }
 
 export default Navbar;
