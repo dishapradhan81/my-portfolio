d1eda758124042d7b169f5d471248f499c005c69
dishapradhan81 <117260610+dishapradhan81@users.noreply.github.com>
2023-09-25 13:06:00 +0530

    Update App.js

--- portfolio/src/Components/App.js
+++ portfolio/src/Components/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Education from "./Education";
 import Projects from "./Projects";
 import Navbar from "../Pages/Navbar";
 import Footer from "../Pages/Footer";
 import ProjectDisplay from "./ProjectDisplay";
-
+import Contact from "./Contact";
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/education" element={<Education />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
